e999bd768e7045acb5dcf89a54811a2775a00471
Edwiler de Lima Araujo <edwillerdelima@gmail.com>
2026-05-28 20:18:24 -0500

    Criando diagramas - REF #02

---
+++ aula1/src/While.java
@@ -0,0 +1,11 @@
+public class While {
+
+    
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 3){
+            System.out.println("Olá " + contador);
+            contador++;
+        }
+    }
+}
